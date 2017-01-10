e336113d5d570618ba79fc47f489f78293025060
Alexandre Biancalana <abiancalana@NI-56879-7.uolcorp.intranet>
2017-01-10 18:57:52 -0200

    cleanup

--- main.go
+++ main.go
@@ -16,26 +16,25 @@ import (
 
 )
 
-//var ( SystemConfig *config.Configuration )
-
 func main() {
 
   var err error
 
   SystemConfig := config.ReadJson("example.json")
 
-  switch SystemConfig.Provider.Name {
-  case "aws":
-    aws.Serializer(SystemConfig)
-  case "openstack":
-    err = ansible.Serializer(SystemConfig)
-    if err != nil {
-      fmt.Println("Failure serializing Ansible Openstack file, ", err)
-    }
+  err = ansible.Serializer(SystemConfig)
+  if err != nil {
+    fmt.Println("Failure serializing Ansible Openstack file, ", err)
+  }
 
-    err = openstack.Serializer(SystemConfig)
-    if err != nil {
-      fmt.Println("Failure serializing Terraform Openstack file, ", err)
-    }
+  switch SystemConfig.Provider.Name {
+    case "aws":
+      aws.Serializer(SystemConfig)
+
+    case "openstack":
+      err = openstack.Serializer(SystemConfig)
+      if err != nil {
+        fmt.Println("Failure serializing Terraform Openstack file, ", err)
+      }
   }
 }
