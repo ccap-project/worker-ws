5f12d7fe8cd12c421d524ba22ddda42e9db9d4ea
biancalana <ale@Ale-MacBook-Pro.local>
2017-01-22 22:32:38 -0200

    Add start point functions to be called from webservice
    while here add some logging

--- terraform/main.go
+++ terraform/main.go
@@ -8,7 +8,7 @@ import (
 
 type Terraform interface {
   Apply(*config.SystemConfig)                   error
-  ReadState(*config.Cell, string)          error
+  ReadState(*config.Cell, string)               error
   Serialize(*config.SystemConfig, *config.Cell) error
   Validate(*config.SystemConfig)                error
 }
@@ -22,7 +22,7 @@ func Init(provider string) Terraform {
   return nil
 }
 
-func Deploy(config *config.SystemConfig, cell *config.Cell) (error) {
+func Check(SystemConfig *config.SystemConfig, cell *config.Cell) (error) {
 
   Env := Init(cell.Provider.Name)
 
@@ -30,19 +30,44 @@ func Deploy(config *config.SystemConfig, cell *config.Cell) (error) {
     return fmt.Errorf("Terraform support for provider(%s) is not implemented ! \n", cell.Provider.Name)
   }
 
-  if err := Env.Serialize(config, cell); err != nil {
+  SystemConfig.Log.Debug("Serializing")
+  if err := Env.Serialize(SystemConfig, cell); err != nil {
     return fmt.Errorf("Failure serializing Terraform Openstack file, %v", err)
   }
 
-  if err := Env.Validate(config); err != nil {
+  SystemConfig.Log.Debug("Validating")
+  if err := Env.Validate(SystemConfig); err != nil {
     return fmt.Errorf("Failure validating Terraform file, %v", err)
   }
 
-  if err := Env.Apply(config); err != nil {
+  return nil
+}
+
+func Deploy(SystemConfig *config.SystemConfig, cell *config.Cell) (error) {
+
+  Env := Init(cell.Provider.Name)
+
+  if Env == nil {
+    return fmt.Errorf("Terraform support for provider(%s) is not implemented ! \n", cell.Provider.Name)
+  }
+
+  SystemConfig.Log.Debug("Serializing")
+  if err := Env.Serialize(SystemConfig, cell); err != nil {
+    return fmt.Errorf("Failure serializing Terraform Openstack file, %v", err)
+  }
+
+  SystemConfig.Log.Debug("Validating")
+  if err := Env.Validate(SystemConfig); err != nil {
+    return fmt.Errorf("Failure validating Terraform file, %v", err)
+  }
+
+  SystemConfig.Log.Debug("Applying")
+  if err := Env.Apply(SystemConfig); err != nil {
     return fmt.Errorf("Failure applying Terraform, %v", err)
   }
 
-  if err := Env.ReadState(cell, config.Files.TerraformState); err != nil {
+  SystemConfig.Log.Debug("Reading state")
+  if err := Env.ReadState(cell, SystemConfig.Files.TerraformState); err != nil {
     return fmt.Errorf("Failure reading Terraform state, %v", err)
   }
 
