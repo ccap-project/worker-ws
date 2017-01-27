a77d927e3b32aa9a92b73bbf1505650eed27b650
biancalana <ale@Ale-MacBook-Pro.local>
2017-01-26 22:36:01 -0200

    fix style using gofmt

--- main.go
+++ main.go
@@ -1,40 +1,41 @@
 package main
 
 import (
-  "flag"
-  "os"
-  log "github.com/Sirupsen/logrus"
-  "./config/"
-  "./webservice"
+	"flag"
+	"os"
+
+	"./config/"
+	"./webservice"
+	log "github.com/Sirupsen/logrus"
 )
 
 var SystemConfig *config.SystemConfig
 
 func main() {
 
-  debugFlag := flag.Bool("debug", false, "enable debug log")
+	debugFlag := flag.Bool("debug", false, "enable debug log")
 
-  flag.Parse()
+	flag.Parse()
 
-  //log.SetFlags(log.LstdFlags | log.Lshortfile)
+	//log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-  //config.Log = log.New(os.Stderr, "config-ws: ", log.LstdFlags | log.Lshortfile)
-  SystemConfig = config.ReadFile("etc/system.conf")
+	//config.Log = log.New(os.Stderr, "config-ws: ", log.LstdFlags | log.Lshortfile)
+	SystemConfig = config.ReadFile("etc/system.conf")
 
-  SystemConfig.Log = log.New()
-  SystemConfig.Log.Out = os.Stderr
+	SystemConfig.Log = log.New()
+	SystemConfig.Log.Out = os.Stderr
 
-  if *debugFlag {
-    SystemConfig.Log.Level = log.DebugLevel
-    SystemConfig.Log.Debug("Log level debug activated")
+	if *debugFlag {
+		SystemConfig.Log.Level = log.DebugLevel
+		SystemConfig.Log.Debug("Log level debug activated")
 
-  } else {
-    SystemConfig.Log.Info("Log level info")
-  }
+	} else {
+		SystemConfig.Log.Info("Log level info")
+	}
 
-  webservice.Start(SystemConfig)
+	webservice.Start(SystemConfig)
 
-  //ReadJson("example.json")
+	//ReadJson("example.json")
 
-  os.Exit(0)
+	os.Exit(0)
 }
