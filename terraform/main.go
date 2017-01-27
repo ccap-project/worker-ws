36c892c1bb93a8c7c084173ec808d6033a81087e
biancalana <ale@Ale-MacBook-Pro.local>
2017-01-26 22:41:43 -0200

    using repoPath, while here fix style

--- terraform/main.go
+++ terraform/main.go
@@ -1,75 +1,76 @@
 package terraform
 
 import (
-  "fmt"
-  "../config/"
-  "./openstack"
+	"fmt"
+
+	"../config/"
+	"./openstack"
 )
 
 type Terraform interface {
-  Apply(*config.SystemConfig)                   error
-  ReadState(*config.Cell, string)               error
-  Serialize(*config.SystemConfig, *config.Cell) error
-  Validate(*config.SystemConfig)                error
+	Apply(*config.SystemConfig, string) error
+	ReadState(*config.Cell, string) error
+	Serialize(*config.SystemConfig, *config.Cell, string) error
+	Validate(*config.SystemConfig, string) error
 }
 
 func Init(provider string) Terraform {
 
-  if provider == "Openstack" {
-    return &openstack.Openstack{}
-  }
+	if provider == "Openstack" {
+		return &openstack.Openstack{}
+	}
 
-  return nil
+	return nil
 }
 
-func Check(SystemConfig *config.SystemConfig, cell *config.Cell) (error) {
+func Check(SystemConfig *config.SystemConfig, cell *config.Cell, repo string) error {
 
-  Env := Init(cell.Provider.Name)
+	Env := Init(cell.Provider.Name)
 
-  if Env == nil {
-    return fmt.Errorf("Terraform support for provider(%s) is not implemented ! \n", cell.Provider.Name)
-  }
+	if Env == nil {
+		return fmt.Errorf("Terraform support for provider(%s) is not implemented ! \n", cell.Provider.Name)
+	}
 
-  SystemConfig.Log.Debug("Serializing")
-  if err := Env.Serialize(SystemConfig, cell); err != nil {
-    return fmt.Errorf("Failure serializing Terraform Openstack file, %v", err)
-  }
+	SystemConfig.Log.Debug("Serializing")
+	if err := Env.Serialize(SystemConfig, cell, repo); err != nil {
+		return fmt.Errorf("Failure serializing Terraform Openstack file, %v", err)
+	}
 
-  SystemConfig.Log.Debug("Validating")
-  if err := Env.Validate(SystemConfig); err != nil {
-    return fmt.Errorf("Failure validating Terraform file, %v", err)
-  }
+	SystemConfig.Log.Debug("Validating")
+	if err := Env.Validate(SystemConfig, repo); err != nil {
+		return fmt.Errorf("Failure validating Terraform file, %v", err)
+	}
 
-  return nil
+	return nil
 }
 
-func Deploy(SystemConfig *config.SystemConfig, cell *config.Cell) (error) {
+func Deploy(SystemConfig *config.SystemConfig, cell *config.Cell, repo string) error {
 
-  Env := Init(cell.Provider.Name)
+	Env := Init(cell.Provider.Name)
 
-  if Env == nil {
-    return fmt.Errorf("Terraform support for provider(%s) is not implemented ! \n", cell.Provider.Name)
-  }
+	if Env == nil {
+		return fmt.Errorf("Terraform support for provider(%s) is not implemented ! \n", cell.Provider.Name)
+	}
 
-  SystemConfig.Log.Debug("Serializing")
-  if err := Env.Serialize(SystemConfig, cell); err != nil {
-    return fmt.Errorf("Failure serializing Terraform Openstack file, %v", err)
-  }
+	SystemConfig.Log.Debug("Serializing")
+	if err := Env.Serialize(SystemConfig, cell, repo); err != nil {
+		return fmt.Errorf("Failure serializing Terraform Openstack file, %v", err)
+	}
 
-  SystemConfig.Log.Debug("Validating")
-  if err := Env.Validate(SystemConfig); err != nil {
-    return fmt.Errorf("Failure validating Terraform file, %v", err)
-  }
+	SystemConfig.Log.Debug("Validating")
+	if err := Env.Validate(SystemConfig, repo); err != nil {
+		return fmt.Errorf("Failure validating Terraform file, %v", err)
+	}
 
-  SystemConfig.Log.Debug("Applying")
-  if err := Env.Apply(SystemConfig); err != nil {
-    return fmt.Errorf("Failure applying Terraform, %v", err)
-  }
+	SystemConfig.Log.Debug("Applying")
+	if err := Env.Apply(SystemConfig, repo); err != nil {
+		return fmt.Errorf("Failure applying Terraform, %v", err)
+	}
 
-  SystemConfig.Log.Debug("Reading state")
-  if err := Env.ReadState(cell, SystemConfig.Files.TerraformState); err != nil {
-    return fmt.Errorf("Failure reading Terraform state, %v", err)
-  }
+	SystemConfig.Log.Debug("Reading state")
+	if err := Env.ReadState(cell, SystemConfig.Files.TerraformState); err != nil {
+		return fmt.Errorf("Failure reading Terraform state, %v", err)
+	}
 
-  return nil
+	return nil
 }
--- terraform/openstack/main.go
+++ terraform/openstack/main.go
@@ -1,14 +1,16 @@
 package openstack
 
 import (
-  "bytes"
-  "io/ioutil"
-  "../../config/"
-  "../common"
-  "../../utils"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+
+	"../../config/"
+	"../../utils"
+	"../common"
 )
 
-type Openstack struct {}
+type Openstack struct{}
 
 const provider_resource_tmpl = `provider "openstack" {
   user_name  = "{{.Username}}"
@@ -17,67 +19,68 @@ const provider_resource_tmpl = `provider "openstack" {
   auth_url  = "{{.AuthUrl}}"
 }
 `
-func (o *Openstack) Apply(system *config.SystemConfig) (error) {
-  return terraformcommon.Apply(system)
+
+func (o *Openstack) Apply(system *config.SystemConfig, dir string) error {
+	return terraformcommon.Apply(system, dir)
 }
 
-func (o *Openstack) Validate(system *config.SystemConfig) (error) {
-  return terraformcommon.Validate(system)
+func (o *Openstack) Validate(system *config.SystemConfig, dir string) error {
+	return terraformcommon.Validate(system, dir)
 }
 
-func (o *Openstack) Serialize(system *config.SystemConfig, cell *config.Cell) (error) {
+func (o *Openstack) Serialize(system *config.SystemConfig, cell *config.Cell, dir string) error {
 
-  var tf bytes.Buffer
+	var tf bytes.Buffer
+	terraformSite := fmt.Sprintf("%s/%s", dir, system.Files.TerraformSite)
 
-  provider, err := provider(cell)
-  if err != nil {
-    return(err)
-  }
+	provider, err := provider(cell)
+	if err != nil {
+		return (err)
+	}
 
-  router, err := router(cell)
-  if err != nil {
-    return(err)
-  }
+	router, err := router(cell)
+	if err != nil {
+		return (err)
+	}
 
-  router_interface, err := router_interface(cell)
-  if err != nil {
-    return(err)
-  }
+	router_interface, err := router_interface(cell)
+	if err != nil {
+		return (err)
+	}
 
-  network, err := network(cell)
-  if err != nil {
-    return(err)
-  }
+	network, err := network(cell)
+	if err != nil {
+		return (err)
+	}
 
-  subnet, err  := subnet(cell)
-  if err != nil {
-    return(err)
-  }
+	subnet, err := subnet(cell)
+	if err != nil {
+		return (err)
+	}
 
-  instance, err  := instance(cell)
-  if err != nil {
-    return(err)
-  }
+	instance, err := instance(cell)
+	if err != nil {
+		return (err)
+	}
 
-  tf.Write(provider.Bytes())
-  tf.Write(router.Bytes())
-  tf.Write(router_interface.Bytes())
-  tf.Write(network.Bytes())
-  tf.Write(subnet.Bytes())
-  tf.Write(instance.Bytes())
+	tf.Write(provider.Bytes())
+	tf.Write(router.Bytes())
+	tf.Write(router_interface.Bytes())
+	tf.Write(network.Bytes())
+	tf.Write(subnet.Bytes())
+	tf.Write(instance.Bytes())
 
-  ioutil.WriteFile(system.Files.TerraformSite, tf.Bytes(), 0644)
+	ioutil.WriteFile(terraformSite, tf.Bytes(), 0644)
 
-  return(nil)
+	return (nil)
 }
 
+func provider(cell *config.Cell) (*bytes.Buffer, error) {
 
-func provider (cell *config.Cell) (*bytes.Buffer, error) {
+	p, err := utils.Template(provider_resource_tmpl, cell.Provider)
+	if err != nil {
+		return p, err
+	}
 
-  p, err := utils.Template(provider_resource_tmpl, cell.Provider)
-  if err != nil {
-    return p,err
-  }
-
-  return p,nil
+	return p, nil
 }
