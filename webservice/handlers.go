f2248bb7c1418f5896fca6254d092ea5c0d695d4
biancalana <ale@Ale-MacBook-Pro.local>
2017-01-26 22:40:59 -0200

    implement makeHandler to handle common validations

--- webservice/handlers.go
+++ webservice/handlers.go
@@ -1,97 +1,108 @@
 package webservice
 
 import (
-  "encoding/json"
-  "fmt"
-  "io"
-  "net/http"
-  "../terraform"
-  "../config"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"../config"
+	"../repo"
+	"../terraform"
 )
 
+func makeHandler(SystemConfig *config.SystemConfig, fn func(http.ResponseWriter, *http.Request, *config.SystemConfig, *config.Cell, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
+		cell, err := config.DecodeJson(io.LimitReader(r.Body, SystemConfig.WebService.BodyLimit))
+		if err != nil {
+			SystemConfig.Log.Error("checkInfrastructure failed, ", err)
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			w.WriteHeader(422) // unprocessable entity
+			if err := json.NewEncoder(w).Encode(err); err != nil {
+				panic(err)
+			}
+		}
+
+		repoPath, err := repo.BuildInfrastructureEnv(SystemConfig, cell.CustomerName, cell.Name)
+		if err != nil {
+			SystemConfig.Log.Error("checkInfrastructure failed, ", err)
+		}
+
+		/*
+			if m == nil {
+				http.NotFound(w, r)
+				return
+			}
+		*/
+
+		fn(w, r, SystemConfig, cell, repoPath)
+	}
+}
+
+/*
 func deploy(SystemConfig *config.SystemConfig) http.HandlerFunc {
 
-  return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-    cell,err := config.DecodeJson(io.LimitReader(r.Body, SystemConfig.WebService.BodyLimit))
-    if err != nil {
-      w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-      w.WriteHeader(422) // unprocessable entity
-      if err := json.NewEncoder(w).Encode(err); err != nil {
-        panic(err)
-      }
-    }
+		cell, err := config.DecodeJson(io.LimitReader(r.Body, SystemConfig.WebService.BodyLimit))
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			w.WriteHeader(422) // unprocessable entity
+			if err := json.NewEncoder(w).Encode(err); err != nil {
+				panic(err)
+			}
+		}
 
-    if err := terraform.Deploy(SystemConfig, cell); err != nil {
-      w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-      w.WriteHeader(422) // unprocessable entity
-      if err := json.NewEncoder(w).Encode(err); err != nil {
-        panic(err)
-      }
-    }
+		if err := terraform.Deploy(SystemConfig, cell); err != nil {
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			w.WriteHeader(422) // unprocessable entity
+			if err := json.NewEncoder(w).Encode(err); err != nil {
+				panic(err)
+			}
+		}
 
-    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-    if err := json.NewEncoder(w).Encode(fmt.Sprintf("Ok")); err != nil {
-      panic(err)
-    }
-  }
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		if err := json.NewEncoder(w).Encode(fmt.Sprintf("Ok")); err != nil {
+			panic(err)
+		}
+	}
+}
+*/
+
+func checkInfrastructure(w http.ResponseWriter, r *http.Request, SystemConfig *config.SystemConfig, cell *config.Cell, repoPath string) {
+
+	SystemConfig.Log.Debugf("running checkInfrastructure CustomerName(%s) cell(%s)", cell.CustomerName, cell.Name)
+
+	if err := terraform.Check(SystemConfig, cell, repoPath); err != nil {
+		SystemConfig.Log.Error("checkInfrastructure failed, ", err)
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	if err := json.NewEncoder(w).Encode(fmt.Sprintf("Ok")); err != nil {
+		panic(err)
+	}
 }
 
-func checkInfrastructure(SystemConfig *config.SystemConfig) http.HandlerFunc {
+func deployInfrastructure(w http.ResponseWriter, r *http.Request, SystemConfig *config.SystemConfig, cell *config.Cell, repoPath string) {
 
-  return func(w http.ResponseWriter, r *http.Request) {
+	SystemConfig.Log.Debugf("running deployInfrastructure CustomerName(%s) cell(%s)", cell.CustomerName, cell.Name)
 
-    cell,err := config.DecodeJson(io.LimitReader(r.Body, SystemConfig.WebService.BodyLimit))
-    if err != nil {
-      SystemConfig.Log.Error("checkInfrastructure failed, ", err)
-      w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-      w.WriteHeader(422) // unprocessable entity
-      if err := json.NewEncoder(w).Encode(err); err != nil {
-        panic(err)
-      }
-    }
+	if err := terraform.Deploy(SystemConfig, cell, repoPath); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
 
-    if err := terraform.Check(SystemConfig, cell); err != nil {
-      SystemConfig.Log.Error("checkInfrastructure failed, ", err)
-      w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-      w.WriteHeader(422) // unprocessable entity
-      if err := json.NewEncoder(w).Encode(err); err != nil {
-        panic(err)
-      }
-    }
-
-    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-    if err := json.NewEncoder(w).Encode(fmt.Sprintf("Ok")); err != nil {
-      panic(err)
-    }
-  }
-}
-
-func deployInfrastructure(SystemConfig *config.SystemConfig) http.HandlerFunc {
-
-  return func(w http.ResponseWriter, r *http.Request) {
-
-    cell,err := config.DecodeJson(io.LimitReader(r.Body, SystemConfig.WebService.BodyLimit))
-    if err != nil {
-      w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-      w.WriteHeader(422) // unprocessable entity
-      if err := json.NewEncoder(w).Encode(err); err != nil {
-        panic(err)
-      }
-    }
-
-    if err := terraform.Deploy(SystemConfig, cell); err != nil {
-      w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-      w.WriteHeader(422) // unprocessable entity
-      if err := json.NewEncoder(w).Encode(err); err != nil {
-        panic(err)
-      }
-    }
-
-    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-    if err := json.NewEncoder(w).Encode(fmt.Sprintf("Ok")); err != nil {
-      panic(err)
-    }
-  }
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	if err := json.NewEncoder(w).Encode(fmt.Sprintf("Ok")); err != nil {
+		panic(err)
+	}
 }
--- webservice/main.go
+++ webservice/main.go
@@ -1,38 +1,39 @@
 package webservice
 
 import (
-  "fmt"
-  "net/http"
-  "os"
-  "time"
-  "github.com/gorilla/handlers"
-  "github.com/gorilla/mux"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
 
-  "../config/"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+
+	"../config/"
 )
 
 func Start(SystemConfig *config.SystemConfig) {
-  main_router := mux.NewRouter()
-  api_router := main_router.PathPrefix(SystemConfig.WebService.ApiPrefix).Subrouter()
+	main_router := mux.NewRouter()
+	api_router := main_router.PathPrefix(SystemConfig.WebService.ApiPrefix).Subrouter()
 
-  // Deploy endpoints
-  api_router.Methods("POST").Path("/deploy").HandlerFunc(deploy(SystemConfig))
+	// Deploy endpoints
+	//api_router.Methods("POST").Path("/deploy").HandlerFunc(deploy(SystemConfig))
 
-  api_router.Methods("POST").Path("/infrastructure/check").HandlerFunc(checkInfrastructure(SystemConfig))
-  api_router.Methods("POST").Path("/infrastructure/deploy").HandlerFunc(deployInfrastructure(SystemConfig))
+	api_router.Methods("POST").Path("/infrastructure/check").HandlerFunc(makeHandler(SystemConfig, checkInfrastructure))
+	api_router.Methods("POST").Path("/infrastructure/check").HandlerFunc(makeHandler(SystemConfig, deployInfrastructure))
 
-  //api_router.Methods("POST").Path("/configuration/check").HandlerFunc(ConfigurationCheck)
-  //api_router.Methods("POST").Path("/configuration/deploy").HandlerFunc(ConfigurationDeploy)
+	//api_router.Methods("POST").Path("/configuration/check").HandlerFunc(ConfigurationCheck)
+	//api_router.Methods("POST").Path("/configuration/deploy").HandlerFunc(ConfigurationDeploy)
 
-  server := &http.Server{
-      Handler:      handlers.CombinedLoggingHandler(os.Stdout, main_router),
-      Addr:         fmt.Sprintf("%s:%d", SystemConfig.WebService.Address, SystemConfig.WebService.Port),
-      // Good practice: enforce timeouts for servers you create!
-      WriteTimeout: 15 * time.Second,
-      ReadTimeout:  15 * time.Second,
-  }
+	server := &http.Server{
+		Handler: handlers.CombinedLoggingHandler(os.Stdout, main_router),
+		Addr:    fmt.Sprintf("%s:%d", SystemConfig.WebService.Address, SystemConfig.WebService.Port),
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
 
-  SystemConfig.Log.Debugf("Starting socket on %s:%d", SystemConfig.WebService.Address, SystemConfig.WebService.Port)
+	SystemConfig.Log.Debugf("Starting socket on %s:%d", SystemConfig.WebService.Address, SystemConfig.WebService.Port)
 
-  SystemConfig.Log.Fatal(server.ListenAndServe())
+	SystemConfig.Log.Fatal(server.ListenAndServe())
 }
