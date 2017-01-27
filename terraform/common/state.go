eeedc9d3225eee896fa9b212dea7da6ae13f5af0
biancalana <ale@Ale-MacBook-Pro.local>
2017-01-26 22:38:46 -0200

    fix style

--- terraform/common/state.go
+++ terraform/common/state.go
@@ -1,25 +1,27 @@
 package terraformcommon
 
 import (
-  "fmt"
-  "os"
-  t "github.com/hashicorp/terraform/terraform"
+	"fmt"
+	"os"
+
+	t "github.com/hashicorp/terraform/terraform"
 )
 
 func ReadState(file string) (*t.State, error) {
 
-  f, err := os.Open(file)
-  defer f.Close()
+	f, err := os.Open(file)
+	defer f.Close()
 
-  if err != nil {
-    fmt.Println("err: ", err)
-    return nil, err
-  }
+	if err != nil {
+		fmt.Println("err: ", err)
+		return nil, err
+	}
 
-  state, err := t.ReadState(f); if err != nil {
-    fmt.Println("err: ", err)
-    return nil, err
-  }
+	state, err := t.ReadState(f)
+	if err != nil {
+		fmt.Println("err: ", err)
+		return nil, err
+	}
 
-  return state,nil
+	return state, nil
 }
--- terraform/common/terraform.go
+++ terraform/common/terraform.go
@@ -1,29 +1,30 @@
 package terraformcommon
 
 import (
-  "errors"
-  "../../config/"
-  "../../utils/"
+	"errors"
+
+	"../../config/"
+	"../../utils/"
 )
 
-func Apply(SystemConfig *config.SystemConfig) error {
+func Apply(SystemConfig *config.SystemConfig, dir string) error {
 
-  cmd,_,stderr := utils.RunCmd(SystemConfig.Commands.Terraform, "apply")
+	cmd, _, stderr := utils.RunCmd(SystemConfig.Commands.Terraform, "apply", dir)
 
-  if err_wait := cmd.Wait(); err_wait != nil {
-    return errors.New(stderr.String())
-  }
+	if err_wait := cmd.Wait(); err_wait != nil {
+		return errors.New(stderr.String())
+	}
 
-  return nil
+	return nil
 }
 
-func Validate(SystemConfig *config.SystemConfig) error {
+func Validate(SystemConfig *config.SystemConfig, dir string) error {
 
-  cmd,_,stderr := utils.RunCmd(SystemConfig.Commands.Terraform, "validate")
+	cmd, _, stderr := utils.RunCmd(SystemConfig.Commands.Terraform, "validate", dir)
 
-  if err_wait := cmd.Wait(); err_wait != nil {
-    return errors.New(stderr.String())
-  }
+	if err_wait := cmd.Wait(); err_wait != nil {
+		return errors.New(stderr.String())
+	}
 
-  return nil
+	return nil
 }
