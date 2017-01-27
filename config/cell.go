2326cc857b95a2d6451f0493ec6c38c9c3776eac
biancalana <ale@Ale-MacBook-Pro.local>
2017-01-26 22:35:27 -0200

    fix style using gofmt

--- config/cell.go
+++ config/cell.go
@@ -1,84 +1,86 @@
 package config
 
 import (
-  "encoding/json"
-  "fmt"
-  "io"
+	"encoding/json"
+	"fmt"
+	"io"
 )
 
 type Provider struct {
-  Name        string `json:"name"`
-  Region      string `json:"region"`
-  Tenantname  string `json:"tenantname"`
-  Username    string `json:"username"`
-  Password    string `json:"password"`
-  AuthUrl     string `json:"auth_url"`
+	Name       string `json:"name"`
+	Region     string `json:"region"`
+	Tenantname string `json:"tenantname"`
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	AuthUrl    string `json:"auth_url"`
 }
 
 type Host struct {
-  Name          string        `json:"name"`
-  Options       []map[string]string `json:"options"`
+	Name    string              `json:"name"`
+	Options []map[string]string `json:"options"`
 }
 
 type Role struct {
-  Name    string `json:"name"`
-  Source  string `json:"src"`
-  Version string `json:"version"`
+	Name    string `json:"name"`
+	Source  string `json:"src"`
+	Version string `json:"version"`
 }
 
 type Hostgroup struct {
-  Name          string `json:"name"`
-  Flavor        string `json:"flavor"`
-  Image         string `json:"image"`
-  Count         string `json:"count"`
-  Network       string `json:"network"`
-  Vars          []map[string]string  `json:"vars"`
-  Roles         []*Role  `json:"roles"`
+	Name    string              `json:"name"`
+	Flavor  string              `json:"flavor"`
+	Image   string              `json:"image"`
+	Count   string              `json:"count"`
+	Network string              `json:"network"`
+	Vars    []map[string]string `json:"vars"`
+	Roles   []*Role             `json:"roles"`
 }
 
 type Network struct {
-  Name          string `json:"name"`
-  AdminState    string `json:"admin_state"`
+	Name       string `json:"name"`
+	AdminState string `json:"admin_state"`
 }
 
 type Router struct {
-  Name          string `json:"name"`
-  AdminState    string `json:"admin_state"`
+	Name       string `json:"name"`
+	AdminState string `json:"admin_state"`
 }
 
 type RouterInterface struct {
-  Name          string `json:"name"`
-  Router        string `json:"router"`
-  Subnet        string `json:"subnet"`
+	Name   string `json:"name"`
+	Router string `json:"router"`
+	Subnet string `json:"subnet"`
 }
 
 type Subnet struct {
-  Name          string `json:"name"`
-  Cidr          string `json:"cidr"`
-  Network       string `json:"network"`
-  IPVersion     string `json:"ip_version"`
-  AdminState    string `json:"admin_state"`
+	Name       string `json:"name"`
+	Cidr       string `json:"cidr"`
+	Network    string `json:"network"`
+	IPVersion  string `json:"ip_version"`
+	AdminState string `json:"admin_state"`
 }
 
 type Cell struct {
-  Provider          *Provider           `json:"provider"`
-  Hosts             []*Host            `json:"hosts"`
-  Hostgroups        []*Hostgroup        `json:"hostgroups"`
-  Networks          []*Network          `json:"networks"`
-  Subnets           []*Subnet           `json:"subnets"`
-  Routers           []*Router           `json:"routers"`
-  RoutersInterfaces []*RouterInterface  `json:"routers_interfaces"`
+	CustomerName      string             `json:"customer_name"`
+	Name              string             `json:"name"`
+	Provider          *Provider          `json:"provider"`
+	Hosts             []*Host            `json:"hosts"`
+	Hostgroups        []*Hostgroup       `json:"hostgroups"`
+	Networks          []*Network         `json:"networks"`
+	Subnets           []*Subnet          `json:"subnets"`
+	Routers           []*Router          `json:"routers"`
+	RoutersInterfaces []*RouterInterface `json:"routers_interfaces"`
 }
 
 func DecodeJson(r io.Reader) (*Cell, error) {
 
-  var config Cell
+	var cell Cell
 
-  decoder := json.NewDecoder(r)
+	decoder := json.NewDecoder(r)
 
-  if err := decoder.Decode(&config); err != nil {
-    return nil, fmt.Errorf("Can't decode request, %s", err)
-  }
+	if err := decoder.Decode(&cell); err != nil {
+		return nil, fmt.Errorf("Can't decode request, %s", err)
+	}
 
-  return &config, nil
+	return &cell, nil
 }
