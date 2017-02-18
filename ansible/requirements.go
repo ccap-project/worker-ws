64c872d5056f125a08e968a511c3c52b3dd3e7c4
biancalana <ale@Ale-MacBook-Pro.local>
2017-02-18 19:46:35 -0200

    default scm to git

--- ansible/requirements.go
+++ ansible/requirements.go
@@ -7,28 +7,29 @@ import "../utils"
 const require_tmpl = `- src: {{.Source}}
   version: {{.Version}}
   name: {{.Name}}
+  scm: git
 
 `
 
-func requirements (config *config.Cell) (*bytes.Buffer, error) {
+func requirements(config *config.Cell) (*bytes.Buffer, error) {
 
-  var requirements bytes.Buffer
+	var requirements bytes.Buffer
 
-  requirements.Write([]byte("---\n"))
+	requirements.Write([]byte("---\n"))
 
-  // XXX: Move this loop to template
-  for _,hostgroup := range config.Hostgroups {
+	// XXX: Move this loop to template
+	for _, hostgroup := range config.Hostgroups {
 
-    if hostgroup.Roles != nil {
-      for _,role := range hostgroup.Roles {
-        req, err := utils.Template(require_tmpl, role)
-        if err != nil {
-          return nil,err
-        }
-        requirements.Write(req.Bytes())
-      }
-    }
-  }
+		if hostgroup.Roles != nil {
+			for _, role := range hostgroup.Roles {
+				req, err := utils.Template(require_tmpl, role)
+				if err != nil {
+					return nil, err
+				}
+				requirements.Write(req.Bytes())
+			}
+		}
+	}
 
-  return &requirements,nil
+	return &requirements, nil
 }
