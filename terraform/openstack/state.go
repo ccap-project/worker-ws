357fcdd69e90d7c95524514dd474e5233c377c3f
biancalana <ale@Ale-MacBook-Pro.local>
2017-02-18 23:29:07 -0200

    fix style

--- terraform/openstack/state.go
+++ terraform/openstack/state.go
@@ -1,37 +1,37 @@
 package openstack
 
 import (
-  "../../config"
-  "../common"
+	"../../config"
+	"../common"
 )
 
-func (o *Openstack) ReadState(cell *config.Cell, file string) (error) {
+func (o *Openstack) ReadState(cell *config.Cell, file string) error {
 
-  state, err := terraformcommon.ReadState(file)
+	state, err := terraformcommon.ReadState(file)
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  for m := range state.Modules {
-    for _,rv := range state.Modules[m].Resources {
+	for m := range state.Modules {
+		for _, rv := range state.Modules[m].Resources {
 
-      switch rv.Type {
-        case "openstack_compute_instance_v2":
-          host := new(config.Host)
-          option := make(map[string]string)
+			switch rv.Type {
+			case "openstack_compute_instance_v2":
+				host := new(config.Host)
+				option := make(map[string]string)
 
-          host.Name = rv.Primary.Attributes["name"]
-          option["ansible_host"] = rv.Primary.Attributes["access_ip_v4"]
+				host.Name = rv.Primary.Attributes["name"]
+				option["ansible_host"] = rv.Primary.Attributes["access_ip_v4"]
 
-          host.Options = append(host.Options, option)
-          cell.Hosts = append(cell.Hosts, host)
+				host.Options = append(host.Options, option)
+				cell.Hosts = append(cell.Hosts, host)
 
-        default:
+			default:
 
-      }
-    }
-  }
+			}
+		}
+	}
 
-  return nil
+	return nil
 }
