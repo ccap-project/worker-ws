c1d2faa027ff6158d77d5c94bd5b16742e9ed374
biancalana <ale@Ale-MacBook-Pro.local>
2017-03-01 16:24:09 -0300

    only write inventory if there is data does not serialize inventory on Deploy()

--- ansible/main.go
+++ ansible/main.go
@@ -12,16 +12,19 @@ func Serializer(config *config.SystemConfig, cell *config.Cell) error {
 
 	var inventory bytes.Buffer
 
-	hosts := hosts(cell)
-	hostgroups := hostgroups(cell)
-	group_vars := group_vars(cell)
+	if hosts := hosts(cell); hosts != nil {
+		inventory.Write(hosts.Bytes())
+	}
 
-	inventory.Write([]byte("---\n"))
-	inventory.Write(hosts.Bytes())
-	inventory.Write(hostgroups.Bytes())
-	inventory.Write(group_vars.Bytes())
+	if hostgroups := hostgroups(cell); hostgroups != nil {
+		inventory.Write(hostgroups.Bytes())
+	}
 
-	ioutil.WriteFile(cell.Environment.Ansible.Dir+config.Files.AnsibleHosts, inventory.Bytes(), 0644)
+	if group_vars := group_vars(cell); group_vars != nil {
+		inventory.Write(group_vars.Bytes())
+	}
+
+	ioutil.WriteFile(GetInventoryFilename(config, cell), inventory.Bytes(), 0644)
 
 	playbook, err := playbook(cell)
 	if err != nil {
@@ -39,6 +42,7 @@ func Serializer(config *config.SystemConfig, cell *config.Cell) error {
 }
 
 func Check(ctx *config.RequestContext) error {
+
 	if err := Serializer(ctx.SystemConfig, ctx.Cell); err != nil {
 		return fmt.Errorf("Failure serializing Ansible Openstack file, %v", err)
 	}
@@ -55,10 +59,6 @@ func Check(ctx *config.RequestContext) error {
 }
 
 func Deploy(ctx *config.RequestContext) error {
-
-	if err := Check(ctx); err != nil {
-		return err
-	}
 
 	if err := Run(ctx.SystemConfig, ctx.Cell); err != nil {
 		return fmt.Errorf("Failure running Ansible, %v", err)
