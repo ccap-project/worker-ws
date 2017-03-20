542027187adbcc0e7ef9d2b7ec7b2a3a1bd52989
biancalana <ale@Ale-MacBook-Pro.local>
2017-03-19 22:58:22 -0300

    bootstrap command support

--- ansible/playbook.go
+++ ansible/playbook.go
@@ -1,33 +1,37 @@
 package ansible
 
 import (
-  "bytes"
+	"bytes"
 
-  "../config"
-  "../utils"
+	"../config"
+	"../utils"
 )
 
-
 const play_tmpl = `{{range .}}{{if .Roles}}- hosts: {{.Name}}
+{{if .BootstrapCommand}}  pre_tasks:
+  - name: Bootstrap Ansible
+    raw: {{.BootstrapCommand}}
+    register: output
+    changed_when: output.stdout != ""
+{{- end}}
   roles:
     {{range .Roles}}- { role: '{{.Name}}', tags: [ '{{.Name}}' ]}{{end}}
 
 {{end}}{{end}}
 `
 
-
 func playbook(config *config.Cell) (*bytes.Buffer, error) {
 
-  var plays bytes.Buffer
+	var plays bytes.Buffer
 
-  plays.Write([]byte("---\n"))
+	plays.Write([]byte("---\n"))
 
-  p, err := utils.Template(play_tmpl, config.Hostgroups)
-  if err != nil {
-    return nil, err
-  }
+	p, err := utils.Template(play_tmpl, config.Hostgroups)
+	if err != nil {
+		return nil, err
+	}
 
-  plays.Write(p.Bytes())
+	plays.Write(p.Bytes())
 
-  return &plays, nil
+	return &plays, nil
 }
