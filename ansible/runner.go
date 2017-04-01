ed2dca614bac9ad2e4e68a3d819356a08c6c54d0
biancalana <ale@Ale-MacBook-Pro.local>
2017-04-01 15:04:25 -0300

    emulate ansible-galaxy update
    check if requirements file where change after roles directory
    and cleanup roles dir if so

--- ansible/runner.go
+++ ansible/runner.go
@@ -2,12 +2,17 @@ package ansible
 
 import (
 	"fmt"
+	"os"
 
 	"../config"
 	"../utils"
 )
 
 func RolesInstall(system *config.SystemConfig, cell *config.Cell) error {
+
+	if needRolesCleanup(system, cell) {
+		clearRoles(system, cell)
+	}
 
 	out, err := utils.RunCmd(cell.Environment.Ansible.Dir, cell.Environment.Ansible.Env, system.Commands.AnsibleGalaxy, "install", "-r", system.Files.AnsibleRequirements)
 
@@ -38,4 +43,30 @@ func SyntaxCheck(system *config.SystemConfig, cell *config.Cell) error {
 	}
 
 	return nil
+}
+
+func needRolesCleanup(system *config.SystemConfig, cell *config.Cell) bool {
+
+	requirements, err := os.Stat(cell.Environment.Ansible.Dir + system.Files.AnsibleRequirements)
+
+	roles, err := os.Stat(cell.Environment.Ansible.Dir + "/roles")
+
+	if err != nil {
+		return true
+	}
+
+	requirements_time := requirements.ModTime()
+
+	if requirements_time.After(roles.ModTime()) {
+		return true
+	}
+
+	return false
+}
+
+func clearRoles(system *config.SystemConfig, cell *config.Cell) error {
+
+	err := os.RemoveAll(cell.Environment.Ansible.Dir + "/roles")
+
+	return err
 }
