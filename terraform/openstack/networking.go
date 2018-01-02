a0f9e315f79b19254e8b084659eb9a8c16fb9156
Alexandre Biancalana <ale@biancalanas.net>
2018-01-02 21:08:02 -0200

    add BSD License ;-)

--- terraform/openstack/networking.go
+++ terraform/openstack/networking.go
@@ -1,3 +1,32 @@
+/*
+ *
+ * Copyright (c) 2016, 2017, 2018 Alexandre Biancalana <ale@biancalanas.net>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 package openstack
 
 import "bytes"
@@ -36,60 +65,60 @@ resource "openstack_networking_subnet_v2" "{{.Name}}" {
 
 func network(config *config.Cell) (*bytes.Buffer, error) {
 
-  var networks bytes.Buffer
+	var networks bytes.Buffer
 
-  for _, net := range config.Networks {
-    n, err := utils.Template(network_resource_tmpl, net)
-    if err != nil {
-      return nil, err
-    }
-    networks.Write(n.Bytes())
-  }
+	for _, net := range config.Networks {
+		n, err := utils.Template(network_resource_tmpl, net)
+		if err != nil {
+			return nil, err
+		}
+		networks.Write(n.Bytes())
+	}
 
-  return &networks, nil
+	return &networks, nil
 }
 
 func router(config *config.Cell) (*bytes.Buffer, error) {
 
-  var routers bytes.Buffer
+	var routers bytes.Buffer
 
-  for _, router := range config.Routers {
+	for _, router := range config.Routers {
 
-    r, err := utils.Template(router_resource_tmpl, router)
-    if err != nil {
-      return nil, err
-    }
-    routers.Write(r.Bytes())
-  }
-  return &routers, nil
+		r, err := utils.Template(router_resource_tmpl, router)
+		if err != nil {
+			return nil, err
+		}
+		routers.Write(r.Bytes())
+	}
+	return &routers, nil
 }
 
 func router_interface(config *config.Cell) (*bytes.Buffer, error) {
 
-  var routers_interfaces bytes.Buffer
+	var routers_interfaces bytes.Buffer
 
-  for _, router_interface := range config.RoutersInterfaces {
-    i,err := utils.Template(router_interface_resource_tmpl, router_interface)
-    if err != nil {
-      return nil, err
-    }
-    routers_interfaces.Write(i.Bytes())
-  }
+	for _, router_interface := range config.RoutersInterfaces {
+		i, err := utils.Template(router_interface_resource_tmpl, router_interface)
+		if err != nil {
+			return nil, err
+		}
+		routers_interfaces.Write(i.Bytes())
+	}
 
-  return &routers_interfaces, nil
+	return &routers_interfaces, nil
 }
 
 func subnet(config *config.Cell) (*bytes.Buffer, error) {
 
-  var subnets bytes.Buffer
+	var subnets bytes.Buffer
 
-  for _, subnet := range config.Subnets {
-    s,err := utils.Template(subnet_resource_tmpl, subnet)
-    if err != nil {
-      return nil, err
-    }
-    subnets.Write(s.Bytes())
-  }
+	for _, subnet := range config.Subnets {
+		s, err := utils.Template(subnet_resource_tmpl, subnet)
+		if err != nil {
+			return nil, err
+		}
+		subnets.Write(s.Bytes())
+	}
 
-  return &subnets, nil
+	return &subnets, nil
 }
